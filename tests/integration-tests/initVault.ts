0595d6f2d42fe637016c7b20d4b88109b1804cd0
dcaf-matcha <93531250+dcaf-matcha@users.noreply.github.com>
2021-11-22 12:22:17 +0400

    isolate integration tests into individual folders (#29)

---
+++ tests/integration-tests/initVault.ts
@@ -0,0 +1,77 @@
+import { web3 } from "@project-serum/anchor";
+import { AccountUtils } from "../utils/AccountUtils";
+import { ExpectUtils } from "../utils/ExpectUtils";
+import { Granularity } from "../utils/Granularity";
+import { KeypairUtils } from "../utils/KeypairUtils";
+import { CONSTANT_SEEDS, PDAUtils } from "../utils/PDAUtils";
+import { ProgramUtils } from "../utils/ProgramUtils";
+import { TokenUtils } from "../utils/TokenUtils";
+import { VaultUtils } from "../utils/VaultUtils";
+
+export function testInitVault() {
+  let vaultProtoConfigAccount: web3.PublicKey;
+
+  beforeEach(async () => {
+    const vaultProtoConfigKeypair = KeypairUtils.generatePair();
+    await VaultUtils.initVaultProtoConfig(vaultProtoConfigKeypair, {
+      granularity: Granularity.DAILY,
+    });
+    vaultProtoConfigAccount = vaultProtoConfigKeypair.publicKey;
+  });
+
+  it('initializes the vault account correctly', async () => {
+    const [tokenA, tokenB] = await Promise.all([
+      TokenUtils.createMockUSDCMint(),
+      TokenUtils.createMockBTCMint(),
+    ]);
+
+    const vaultPDA = await PDAUtils.getVaultPDA(
+      tokenA.publicKey,
+      tokenB.publicKey,
+      vaultProtoConfigAccount,
+    );
+
+    const [vaultTokenAAccountPDA, vaultTokenBAccountPDA] = await Promise.all([
+      PDAUtils.getTokenAccountPDA(
+        ProgramUtils.vaultProgram.programId,
+        CONSTANT_SEEDS.tokenAAccount,
+        vaultPDA.pubkey,
+        tokenA.publicKey,
+      ),
+      PDAUtils.getTokenAccountPDA(
+        ProgramUtils.vaultProgram.programId,
+        CONSTANT_SEEDS.tokenBAccount,
+        vaultPDA.pubkey,
+        tokenB.publicKey,
+      )
+    ]); 
+
+    await VaultUtils.initVault(
+      vaultPDA,
+      vaultProtoConfigAccount,
+      tokenA.publicKey,
+      tokenB.publicKey,
+      vaultTokenAAccountPDA,
+      vaultTokenBAccountPDA,
+    );
+
+    const vaultAccount = await AccountUtils.fetchVaultAccount(vaultPDA.pubkey);
+
+    const [vaultTokenAAccount, vaultTokenBAccount] = await Promise.all([
+      TokenUtils.fetchTokenAccountInfo(vaultAccount.tokenAAccount),
+      TokenUtils.fetchTokenAccountInfo(vaultAccount.tokenBAccount),
+    ]);
+
+    ExpectUtils.batchExpectPubkeysToBeEqual(
+      [vaultAccount.protoConfig, vaultProtoConfigAccount],
+      [vaultAccount.tokenAMint, tokenA.publicKey],
+      [vaultAccount.tokenBMint, tokenB.publicKey],
+      [vaultAccount.tokenAAccount, vaultTokenAAccountPDA.pubkey],
+      [vaultAccount.tokenBAccount, vaultTokenBAccountPDA.pubkey],
+      [vaultTokenAAccount.mint, tokenA.publicKey],
+      [vaultTokenBAccount.mint, tokenB.publicKey],
+      [vaultTokenAAccount.owner, vaultPDA.pubkey],
+      [vaultTokenBAccount.owner, vaultPDA.pubkey],
+    );
+  });
+}
