df5d521489f63f816ebffc4f4cc18ec15f06e3e7
matcha <matchadcaf@protonmail.com>
2021-12-05 16:27:30 +0400

    run cargo fmt

--- programs/dca-vault/src/state/vault_period.rs
+++ programs/dca-vault/src/state/vault_period.rs
@@ -1,15 +1,15 @@
-use anchor_lang::prelude::*;
 use super::traits::ByteSized;
+use anchor_lang::prelude::*;
 
 #[account]
 #[derive(Default)]
 pub struct VaultPeriod {
-  // Account relations
-  pub vault: Pubkey,
+    // Account relations
+    pub vault: Pubkey,
 
-  // Data
-  pub twap: u64, // Time weighted average price of asset A expressed in asset B from period 1 to this period
-  pub dar: u64, // Drip amount to reduce at this period
+    // Data
+    pub twap: u64, // Time weighted average price of asset A expressed in asset B from period 1 to this period
+    pub dar: u64,  // Drip amount to reduce at this period
 }
 
 impl ByteSized for VaultPeriod {}
