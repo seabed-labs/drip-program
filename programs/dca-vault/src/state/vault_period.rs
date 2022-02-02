002d67c9349e11641ad1b0ddb66f49f6116f2bd5
matcha <matchadcaf@protonmail.com>
2022-02-01 22:54:15 -0500

    vault period account done

--- programs/dca-vault/src/state/vault_period.rs
+++ programs/dca-vault/src/state/vault_period.rs
@@ -8,6 +8,7 @@ pub struct VaultPeriod {
     pub vault: Pubkey,
 
     // Data
+    pub index: u64, // The period index/offset from the genesis period of the vault (0, 1, ...)
     pub twap: u64, // Time weighted average price of asset A expressed in asset B from period 1 to this period
     pub dar: u64,  // Drip amount to reduce at this period
 }
@@ -20,6 +21,6 @@ mod test {
 
     #[test]
     fn sanity_check_byte_size() {
-        assert_eq!(VaultPeriod::byte_size(), 48);
+        assert_eq!(VaultPeriod::byte_size(), 56);
     }
 }
