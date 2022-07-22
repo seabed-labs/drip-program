2558da5b02102afd05e9835627284ceaf0b08ec3
Mocha Dcaf <mocha@dcaf.so>
2022-07-22 16:34:15 -0400

    chore: fix parallel tests

--- tests/integration-tests/program.ts
+++ tests/integration-tests/program.ts
@@ -3,7 +3,7 @@ import { web3 } from "@project-serum/anchor";
 import { AccountUtil } from "../utils/Account.util";
 import { ProgramUtil } from "../utils/Program.util";
 
-describe("Test Dependent Programs", () => {
+describe("Test Dependent Programs", async () => {
   it("should have the token swap program", async () => {
     const tokenSwapProgram = await AccountUtil.fetchAccountInfo(
       new web3.PublicKey(ProgramUtil.tokenSwapProgram.programId)
