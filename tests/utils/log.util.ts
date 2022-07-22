ccc9823d8d25c3e7a82b83d904b3cf1abc7f2a38
Mocha Dcaf <mocha@dcaf.so>
2022-07-22 17:12:42 -0400

    chore: dont double wrap log

--- tests/utils/log.util.ts
+++ tests/utils/log.util.ts
@@ -1,12 +1,15 @@
 import sinon from "sinon";
 
 export function initLog() {
-  const DISABLE_LOGGING = !process.env.LOG;
-  if (DISABLE_LOGGING) {
-    console.log("DISABLED LOGGING");
-    sinon.stub(console, "log");
-    sinon.stub(console, "error");
-    sinon.stub(console, "warn");
-    sinon.stub(console, "info");
+  try {
+    const DISABLE_LOGGING = !process.env.LOG;
+    if (DISABLE_LOGGING) {
+      console.log("DISABLED LOGGING");
+      sinon.stub(console, "log");
+      sinon.stub(console, "error");
+      sinon.stub(console, "warn");
+      sinon.stub(console, "info");
+    }
+  } finally {
   }
 }
