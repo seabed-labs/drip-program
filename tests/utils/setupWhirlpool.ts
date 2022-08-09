f826ac0668cce3032c341c3b121f49e38c8d4859
Mocha Dcaf <mocha@dcaf.so>
2022-08-08 22:57:48 -0400

    feat: initialzie mpl token metadata account for dev tokens

--- tests/utils/setupWhirlpool.ts
+++ tests/utils/setupWhirlpool.ts
@@ -14,6 +14,7 @@ import {
   WhirlpoolIx,
 } from "@orca-so/whirlpools-sdk";
 import { ProgramUtil } from "./program.util";
+import { amount, Denom } from "./common.util";
 
 describe("#setupWhirlpool", setupWhirlpool);
 
