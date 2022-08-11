b0f91329995d543af2b02c72992bacb07ab6186b
Mocha Dcaf <mocha@dcaf.so>
2022-08-10 23:17:52 -0400

    chore: update script

--- tests/utils/setupMPLTokenMetadata.ts
+++ tests/utils/setupMPLTokenMetadata.ts
@@ -1,7 +1,10 @@
 import "should";
 import { Keypair, PublicKey, Transaction } from "@solana/web3.js";
 import { PDAUtil } from "@orca-so/whirlpools-sdk";
-import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
+import {
+  createCreateMetadataAccountV3Instruction,
+  createSetTokenStandardInstruction,
+} from "@metaplex-foundation/mpl-token-metadata";
 import { TestUtil } from "./config.util";
 
 describe("#setupTokenMetadata", setupTokenMetadata);
@@ -34,57 +37,71 @@ export function setupTokenMetadata() {
       btcPositionMetadataAccount.toBase58()
     );
 
-    const usdtIx = await createCreateMetadataAccountV3Instruction(
-      {
-        metadata: usdtPositionMetadataAccount,
-        mint: usdtPubkey,
-        mintAuthority: tokenOwnerKeypair.publicKey,
-        payer: TestUtil.provider.wallet.publicKey,
-        updateAuthority: tokenOwnerKeypair.publicKey,
-      },
-      {
-        createMetadataAccountArgsV3: {
-          data: {
-            name: "USDT (Drip)",
-            symbol: "USDT",
-            uri: "https://devnet.api.drip.dcaf.so/v1/drip/8ULDKGmKJJaZa32eiL36ARr6cFaZaoAXAosWeg5r17ra/tokenmetadata",
-            sellerFeeBasisPoints: 0,
-            creators: undefined,
-            collection: undefined,
-            uses: undefined,
-          },
-          isMutable: true,
-          collectionDetails: undefined,
-        },
-      }
-    );
+    // const usdtIx = await createCreateMetadataAccountV3Instruction(
+    //   {
+    //     metadata: usdtPositionMetadataAccount,
+    //     mint: usdtPubkey,
+    //     mintAuthority: tokenOwnerKeypair.publicKey,
+    //     payer: TestUtil.provider.wallet.publicKey,
+    //     updateAuthority: tokenOwnerKeypair.publicKey,
+    //   },
+    //   {
+    //     createMetadataAccountArgsV3: {
+    //       data: {
+    //         name: "USDT (Drip)",
+    //         symbol: "USDT",
+    //         uri: "https://devnet.api.drip.dcaf.so/v1/drip/8ULDKGmKJJaZa32eiL36ARr6cFaZaoAXAosWeg5r17ra/tokenmetadata",
+    //         sellerFeeBasisPoints: 0,
+    //         creators: undefined,
+    //         collection: undefined,
+    //         uses: undefined,
+    //       },
+    //       isMutable: true,
+    //       collectionDetails: undefined,
+    //     },
+    //   }
+    // );
+    const usdtTokenStandardIx = createSetTokenStandardInstruction({
+      metadata: usdtPositionMetadataAccount,
+      updateAuthority: tokenOwnerKeypair.publicKey,
+      mint: usdtPubkey,
+    });
     // createMetadataAccountV2
 
-    const btcIx = await createCreateMetadataAccountV3Instruction(
-      {
-        metadata: btcPositionMetadataAccount,
-        mint: btcPubkey,
-        mintAuthority: tokenOwnerKeypair.publicKey,
-        payer: TestUtil.provider.wallet.publicKey,
-        updateAuthority: tokenOwnerKeypair.publicKey,
-      },
-      {
-        createMetadataAccountArgsV3: {
-          data: {
-            name: "BTC (Drip)",
-            symbol: "BTC",
-            uri: "https://devnet.api.drip.dcaf.so/v1/drip/5nY3xT4PJe7NU41zqBx5UACHDckrimmfwznv4uLenrQg/tokenmetadata",
-            sellerFeeBasisPoints: 0,
-            creators: undefined,
-            collection: undefined,
-            uses: undefined,
-          },
-          isMutable: true,
-          collectionDetails: undefined,
-        },
-      }
-    );
-    const tx = new Transaction().add(usdtIx).add(btcIx);
+    // const btcIx = await createCreateMetadataAccountV3Instruction(
+    //   {
+    //     metadata: btcPositionMetadataAccount,
+    //     mint: btcPubkey,
+    //     mintAuthority: tokenOwnerKeypair.publicKey,
+    //     payer: TestUtil.provider.wallet.publicKey,
+    //     updateAuthority: tokenOwnerKeypair.publicKey,
+    //   },
+    //   {
+    //     createMetadataAccountArgsV3: {
+    //       data: {
+    //         name: "BTC (Drip)",
+    //         symbol: "BTC",
+    //         uri: "https://devnet.api.drip.dcaf.so/v1/drip/5nY3xT4PJe7NU41zqBx5UACHDckrimmfwznv4uLenrQg/tokenmetadata",
+    //         sellerFeeBasisPoints: 0,
+    //         creators: undefined,
+    //         collection: undefined,
+    //         uses: undefined,
+    //       },
+    //       isMutable: true,
+    //       collectionDetails: undefined,
+    //     },
+    //   }
+    // );
+    const btcTokenStandardIx = createSetTokenStandardInstruction({
+      metadata: btcPositionMetadataAccount,
+      updateAuthority: tokenOwnerKeypair.publicKey,
+      mint: btcPubkey,
+    });
+
+    // const tx = new Transaction().add(usdtIx).add(btcIx);
+    const tx = new Transaction()
+      .add(usdtTokenStandardIx)
+      .add(btcTokenStandardIx);
     const txId = await TestUtil.provider.sendAndConfirm(tx, [
       tokenOwnerKeypair,
     ]);
