f7543f4a0c90d3dcb51acfb1409d5a714f66f5bc
matcha <matchadcaf@protonmail.com>
2022-08-26 18:31:38 -0700

    more stuff

--- programs/drip/src/interactions/mod.rs
+++ programs/drip/src/interactions/mod.rs
@@ -1,7 +1,7 @@
 pub mod burn_token;
+pub mod create_token_metadata;
 pub mod deposit_utils;
 pub mod drip_utils;
 pub mod mint_token;
 pub mod transfer_token;
 pub mod withdraw_utils;
-pub mod create_token_metadata;
