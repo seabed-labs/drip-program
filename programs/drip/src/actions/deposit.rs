0206b88bc631640474364f3cceae559ce08757bb
Mocha Dcaf <mocha@dcaf.so>
2022-08-31 00:47:21 -0400

    fix: rename instruction_accounts files to pass anchor build

--- programs/drip/src/actions/deposit.rs
+++ programs/drip/src/actions/deposit.rs
@@ -1,7 +1,9 @@
 use anchor_lang::prelude::*;
 
 use crate::{
-    instruction_accounts::deposit::{DepositAccounts, DepositParams, DepositWithMetadataAccounts},
+    instruction_accounts::ia_deposit::{
+        DepositAccounts, DepositParams, DepositWithMetadataAccounts,
+    },
     state::traits::{Executable, Validatable},
 };
 
