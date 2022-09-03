f38025bd7bb47fd707db49b56a34579214314cdc
Mocha Dcaf <mocha@dcaf.so>
2022-09-03 01:02:46 -0300

    feat: add rust unit test for init_vault_proto_config validate

--- programs/drip/src/actions/init.rs
+++ programs/drip/src/actions/init.rs
@@ -94,3 +94,73 @@ fn init_vault_period(
     msg!("Initialized VaultPeriod");
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::state;
+    use crate::Init::VaultProtoConfig;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    //  TODO: clean this up
+    #[test]
+    fn test_init_vault_proto_config() {
+        let key: Pubkey = Default::default();
+        let mut lamports: u64 = 1;
+        let owner: Pubkey = Default::default();
+        let default_account_info = &AccountInfo {
+            key: &key,
+            is_signer: false,
+            is_writable: false,
+            lamports: Rc::new(RefCell::new(&mut lamports)),
+            data: Rc::new(RefCell::new(&mut [])),
+            owner: &owner,
+            executable: false,
+            rent_epoch: 0,
+        };
+
+        let mut signer_account = default_account_info.clone();
+        signer_account.is_signer = true;
+
+        let mut system_program_account = default_account_info.clone();
+        system_program_account.executable = true;
+
+        let mut vault_proto_config_account = default_account_info.clone();
+
+        let vault_proto_config_key: Pubkey = Pubkey::new_unique();
+        vault_proto_config_account.key = &vault_proto_config_key;
+
+        let vault_proto_config_data: state::VaultProtoConfig = state::VaultProtoConfig {
+            granularity: 0,
+            token_a_drip_trigger_spread: 0,
+            token_b_withdrawal_spread: 0,
+            admin: Default::default(),
+        };
+        let mut buf = Vec::new();
+        vault_proto_config_data.try_serialize(&mut buf).unwrap();
+        vault_proto_config_account.data = Rc::new(RefCell::new(&mut buf));
+
+        vault_proto_config_account.owner = &crate::ID;
+
+        let initialize_vault_proto_config_accounts = &mut InitializeVaultProtoConfigAccounts {
+            creator: Signer::try_from(&signer_account).unwrap(),
+            vault_proto_config: Account::try_from(&vault_proto_config_account).unwrap(),
+            system_program: Program::try_from(&system_program_account).unwrap(),
+        };
+
+        let initialize_vault_proto_config_params = InitializeVaultProtoConfigParams {
+            granularity: 0,
+            token_a_drip_trigger_spread: 0,
+            token_b_withdrawal_spread: 0,
+            admin: Default::default(),
+        };
+
+        let vault_proto_config_action = VaultProtoConfig {
+            accounts: initialize_vault_proto_config_accounts,
+            params: initialize_vault_proto_config_params,
+        };
+        let res = vault_proto_config_action.validate();
+        assert_eq!(res, Err(ErrorCode::InvalidGranularity.into()));
+    }
+}
