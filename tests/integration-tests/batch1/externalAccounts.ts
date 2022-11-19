07abed10422c49d0e0a219183a0e420447b1405a
Mocha Dcaf <mocha@dcaf.so>
2022-11-18 20:19:11 -0500

    feat: clone eth and usdc accounts to local tests

---
+++ tests/integration-tests/batch1/externalAccounts.ts
@@ -0,0 +1,69 @@
+import "should";
+import { web3 } from "@project-serum/anchor";
+import { AccountUtil } from "../../utils/account.util";
+import { ProgramUtil } from "../../utils/program.util";
+import { parsePriceData, parseProductData } from "../../utils/pyth";
+
+describe("Test External Accounts", () => {
+  it("should have the token swap program", async () => {
+    const tokenSwapProgram = await AccountUtil.fetchAccountInfo(
+      new web3.PublicKey(ProgramUtil.tokenSwapProgram.programId)
+    );
+    tokenSwapProgram.should.not.be.undefined();
+  });
+
+  it("should have the orca whirlpool program", async () => {
+    const orcaWhirlpoolProgram = await AccountUtil.fetchAccountInfo(
+      new web3.PublicKey(ProgramUtil.orcaWhirlpoolProgram.programId)
+    );
+    orcaWhirlpoolProgram.should.not.be.undefined();
+  });
+
+  it("should have the metaplex token metadata program", async () => {
+    const metadataProgram = await AccountUtil.fetchAccountInfo(
+      new web3.PublicKey(ProgramUtil.metadataProgram.programId)
+    );
+    metadataProgram.should.not.be.undefined();
+  });
+
+  it("should have the pyth program", async () => {
+    const pythProgram = await AccountUtil.fetchAccountInfo(
+      new web3.PublicKey(ProgramUtil.pythProgram.programId)
+    );
+    pythProgram.should.not.be.undefined();
+  });
+
+  it("should have the pyth ETH price account", async () => {
+    let info = await AccountUtil.fetchAccountInfo(
+      new web3.PublicKey("JBu1AL4obBcCMqKBBxhpWCNUt136ijcuMZLFvTP7iWdB")
+    );
+    const priceData = parsePriceData(info.data as any);
+    priceData.productAccountKey
+      .toString()
+      .should.equal("EMkxjGC1CQ7JLiutDbfYb7UKb3zm9SJcUmr1YicBsdpZ");
+
+    info = await AccountUtil.fetchAccountInfo(
+      new web3.PublicKey("EMkxjGC1CQ7JLiutDbfYb7UKb3zm9SJcUmr1YicBsdpZ")
+    );
+    const productData = parseProductData(info.data as any);
+    productData.product["quote_currency"].should.equal("USD");
+    productData.product["base"].should.equal("ETH");
+  });
+
+  it("should have the pyth USDC price account", async () => {
+    let info = await AccountUtil.fetchAccountInfo(
+      new web3.PublicKey("Gnt27xtC473ZT2Mw5u8wZ68Z3gULkSTb5DuxJy7eJotD")
+    );
+    const priceData = parsePriceData(info.data as any);
+    priceData.productAccountKey
+      .toString()
+      .should.equal("8GWTTbNiXdmyZREXbjsZBmCRuzdPrW55dnZGDkTRjWvb");
+
+    info = await AccountUtil.fetchAccountInfo(
+      new web3.PublicKey("8GWTTbNiXdmyZREXbjsZBmCRuzdPrW55dnZGDkTRjWvb")
+    );
+    const productData = parseProductData(info.data as any);
+    productData.product["quote_currency"].should.equal("USD");
+    productData.product["base"].should.equal("USDC");
+  });
+});
